95b80c422f48f74ee8de1f351de131b64552623f
granlite <65130690+granlite@users.noreply.github.com>
2020-09-15 23:02:33 +0400

    Add files via upload

---
+++ mvc6/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace muscshop.Models
+{
+    public class User
+    {
+       
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public bool Active { get; set; } = false;
+        public Guid PassRecovery { get; set; }
+        public Guid Confirmation { get; set; }
+    }
+}
